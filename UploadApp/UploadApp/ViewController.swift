eab2fe5170278b91150083239384236bee915f0e
Yocelin Garcia <garcia_romero.y@hotmail.com>
2021-04-03 14:25:24 -0600

    add view design with all the element to be used

---
+++ UploadApp/UploadApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UploadApp
+//
+//  Created by Yocelin Garcia Romero on 03/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
