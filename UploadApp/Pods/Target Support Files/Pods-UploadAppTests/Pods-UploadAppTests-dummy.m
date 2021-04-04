189e83f1131aed9971f6ac9134bdf9b0f27692a2
Yocelin Garcia <garcia_romero.y@hotmail.com>
2021-04-04 03:20:49 -0500

    Add upload and download functions

---
+++ UploadApp/Pods/Target Support Files/Pods-UploadAppTests/Pods-UploadAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UploadAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_UploadAppTests
+@end
--- UploadApp/UploadApp/ViewController.swift
+++ UploadApp/UploadApp/ViewController.swift
@@ -6,13 +6,24 @@
 //
 
 import UIKit
+import Firebase
+import CoreServices
+import FirebaseUI
 
 class ViewController: UIViewController {
-    var currentImage = 0
-    let photoList = [UIImage(named: "1"), UIImage(named: "2"), UIImage(named: "3"), UIImage(named: "4"), UIImage(named: "5")]
+    // Outlets
     @IBOutlet weak var ImageCarousel: UIImageView!
+    @IBOutlet weak var uploadButton: UIButton!
     @IBOutlet weak var LeftOut: UIButton!
     
+    // Variables
+    var currentImage: Int = 0
+    let photoList: Array = [UIImage(named: "1"), UIImage(named: "2"), UIImage(named: "3"), UIImage(named: "4"), UIImage(named: "5")]
+    let placeholerImage = UIImage(named: "placeholder")
+    
+    var images: [StorageReference] = []
+    let storage = Storage.storage()
+   
     
     
 
@@ -20,6 +31,18 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         ImageCarousel.image = photoList[currentImage]
         LeftOut.isEnabled = false
+        
+        downloadImage()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear")
     }
 
     @IBAction func RightAction(_ sender: Any) {
@@ -41,5 +64,105 @@ class ViewController: UIViewController {
             LeftOut.isEnabled = false
         }
     }
+    
+    @IBAction func uploadImage(_ sender: Any) {
+        let userImagePicker = UIImagePickerController()
+        userImagePicker.delegate = self
+        userImagePicker.sourceType = .photoLibrary
+        present(userImagePicker, animated: true, completion: nil)
+    }
+    
+    func uploadImage(imageData: Data){
+        let activityIndicator = UIActivityIndicatorView.init(style: .large)
+        activityIndicator.startAnimating()
+        activityIndicator.center = self.view.center
+        self.view.addSubview(activityIndicator)
+        let imagesCount = images.count
+        let storageRef = storage.reference()
+        let imageRef = storageRef.child("uploadapp").child("E0JQZWeWPrYvDUAF98QLanwUurk1").child("\(imagesCount + 1).jpg")
+        
+        // metadata
+        let uploadMetaData = StorageMetadata()
+        uploadMetaData.contentType = "image/jpeg"
+        // data de la foto (buffer de foto) y tipo de datos de la imagen
+        imageRef.putData(imageData, metadata: uploadMetaData) { (metadata, error) in
+            activityIndicator.stopAnimating()
+            activityIndicator.removeFromSuperview()
+            
+            if let error = error{
+                print(error.localizedDescription)
+            } else{
+                print("Image metadata: \(String(describing: metadata))")
+            }
+        }
+    }
+    
+    func downloadImage(){
+        currentImage += 1
+        let storageRef = storage.reference()
+        let imageDownloadUrlRef = storageRef.child("uploadapp/E0JQZWeWPrYvDUAF98QLanwUurk1/\(currentImage).jpg")
+        
+        images.append(imageDownloadUrlRef)
+        
+        
+        imageDownloadUrlRef.downloadURL { (url, error) in
+            if let error = error{
+                print(error.localizedDescription)
+            } else {
+                print("URL: \(String(describing: url!))")
+            }
+            
+        }
+    }
 }
+
+extension ViewController: UIImagePickerControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage, let optimizeImageData = userImage.jpegData(compressionQuality: 0.6){
+            uploadImage(imageData: optimizeImageData)
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension ViewController: UINavigationControllerDelegate{
+    
+}
+
+/*extension ViewController: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "imageCellXIB", for: indexPath) as! ImageCollectionViewCell
+        
+        
+        let ref = images[indexPath.item]
+        
+        cell.imageViewCell.sd_setImage(with: ref, placeholderImage: placeholerImage)
+        
+        ref.downloadURL { (url, error) in
+            if let error = error{
+                print(error.localizedDescription)
+            } else {
+                print("URL: \(String(describing: url!))")
+            }
+            
+        }
+        
+        return cell
+    }
+    
+    
+}
+
+extension ViewController: UICollectionViewDelegate{
+    
+}
+// maneja lo referente a
+extension ViewController: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 100, height: 100)
+    }
+}*/
 
