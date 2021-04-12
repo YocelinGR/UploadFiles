fdca61c346a7475ad12aa2499763e69b1f06d516
YocelinGR <39833045+YocelinGR@users.noreply.github.com>
2021-04-11 19:22:12 -0500

    Fix the delete functionality and the movement between images (#3)

---
+++ UploadApp/Pods/Target Support Files/Pods-UploadApp-UploadAppUITests/Pods-UploadApp-UploadAppUITests-dummy 2.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UploadApp_UploadAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_UploadApp_UploadAppUITests
+@end
---
+++ UploadApp/UploadApp/DetailViewController 2.swift
@@ -0,0 +1,71 @@
+//
+//  DetailViewController.swift
+//  UploadApp
+//
+//  Created by Yocelin Garcia Romero on 04/04/21.
+//
+
+import UIKit
+import Firebase
+import CoreServices
+import FirebaseUI
+
+class DetailViewController: UIViewController {
+    
+    var imageRef: StorageReference
+    let placeholerImage = UIImage(named: "placeholder")
+    let storage = Storage.storage()
+    
+    required init?(coder aDecoder: NSCoder) {
+        let storageRef = storage.reference()
+        self.imageRef = storageRef.child("images/profile/userProfile.png")
+        super.init(coder: aDecoder)
+    }
+    
+    
+    @IBOutlet weak var detailImage: UIImageView!
+    @IBOutlet weak var imageName: UILabel!
+    @IBOutlet weak var imageDatetime: UILabel!
+    @IBOutlet weak var imageSize: UILabel!
+    @IBOutlet weak var likeCount: UILabel!
+    
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var deleteButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setImageValues()
+        // Do any additional setup after loading the view.
+    }
+    
+    func setImageValues(){
+        let forestRef = imageRef
+        forestRef.getMetadata { metadata, error in
+          if let error = error {
+            print("An error had happended: \(error)")
+          } else {
+            print("wwwwwwwttttt: \(String(describing: metadata))")
+            self.detailImage.sd_setImage(with: self.imageRef, placeholderImage: self.placeholerImage)
+            self.imageName.text = "Nombre: \(metadata?.name ?? "")"
+            self.imageDatetime.text = "Fecha: \(metadata?.timeCreated ?? Date())"
+            self.imageSize.text = "Size: \(metadata?.size ?? 0)"
+          }
+        }
+    }
+    
+    @IBAction func likeOrDislike(_ sender: Any) {
+    }
+    
+    @IBAction func deleteImage(_ sender: Any) {
+        imageRef.delete { error in
+          if let error = error {
+            print("An error had happended: \(error)")
+          } else {
+            print("Sucess!!")
+          }
+        }
+    }
+    
+}
+
